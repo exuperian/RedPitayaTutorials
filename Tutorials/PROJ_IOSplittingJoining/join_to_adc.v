dd2bf94401c8ee7207e3a1e76fe77733fc0dee08
Ruvi Lecamwasam <ruvileca@gmail.com>
2023-06-05 22:14:18 +0900

    Finished split joined tutorial images and block design

--- Tutorials/PROJ_IOSplittingJoining/join_to_adc.v
+++ Tutorials/PROJ_IOSplittingJoining/join_to_adc.v
@@ -16,9 +16,9 @@ module join_to_adc #
   (* X_INTERFACE_PARAMETER = "FREQ_HZ 125000000" *)
   input wire                        aclk,
   (* X_INTERFACE_PARAMETER = "FREQ_HZ 125000000" *)  
-  input wire [PADDED_DATA_WIDTH-1:0] o_data_a,
+  input wire [PADDED_DATA_WIDTH-1:0] out_1,
   (* X_INTERFACE_PARAMETER = "FREQ_HZ 125000000" *)
-  input wire [PADDED_DATA_WIDTH-1:0] o_data_b,
+  input wire [PADDED_DATA_WIDTH-1:0] out_2,
   input wire t_valid,
   
   //Joined data output
@@ -33,7 +33,7 @@ module join_to_adc #
     begin
         if(t_valid)
         begin
-            int_tdata_reg <= {o_data_b[PADDED_DATA_WIDTH-1:0],o_data_a[PADDED_DATA_WIDTH-1:0]};
+            int_tdata_reg <= {out_2[PADDED_DATA_WIDTH-1:0],out_1[PADDED_DATA_WIDTH-1:0]};
         end
     end
 
--- Tutorials/PROJ_IOSplittingJoining/split_from_dac.v
+++ Tutorials/PROJ_IOSplittingJoining/split_from_dac.v
@@ -21,26 +21,26 @@ module split_from_dac #
   
   //Split data output
   (* X_INTERFACE_PARAMETER = "FREQ_HZ 125000000" *)  
-  output wire [PADDED_DATA_WIDTH-1:0] o_data_a,
+  output wire [PADDED_DATA_WIDTH-1:0] in_1,
   (* X_INTERFACE_PARAMETER = "FREQ_HZ 125000000" *)
-  output wire [PADDED_DATA_WIDTH-1:0] o_data_b,
+  output wire [PADDED_DATA_WIDTH-1:0] in_2,
   output wire t_valid
 );
 
-  reg  [PADDED_DATA_WIDTH-1:0] int_data_a_reg;
-  reg  [PADDED_DATA_WIDTH-1:0] int_data_b_reg;
+  reg  [PADDED_DATA_WIDTH-1:0] in_1_reg;
+  reg  [PADDED_DATA_WIDTH-1:0] in_2_reg;
 
   always @(posedge aclk)
   begin
     if(s_axis_tvalid)
     begin
-        int_data_a_reg <= s_axis_tdata[15:0];
-        int_data_b_reg <= s_axis_tdata[31:16];
+        in_1_reg <= s_axis_tdata[15:0];
+        in_2_reg <= s_axis_tdata[31:16];
     end
   end
 
-  assign o_data_a = int_data_a_reg;
-  assign o_data_b = int_data_b_reg;
+  assign in_1 = in_1_reg;
+  assign in_2 = in_2_reg;
 
   assign t_valid  = s_axis_tvalid;
 
